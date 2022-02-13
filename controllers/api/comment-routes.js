06e240d97592675df6f1ef1b04ad982c3e5f15ba
unknown <seancollins117@gmail.com>
2022-02-13 08:52:20 -0800

    handlebar templates recieve database data

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -10,6 +10,7 @@ router.get('/', (req, res) => {
     });
 });
 
+
 router.post('/', (req, res) => {
   // expects => {comment_text: "This is the comment", user_id: 1, post_id: 2}
   Comment.create({
