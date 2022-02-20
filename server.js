e32e0bf02cbd09b9ec352e30e5dcace334933ab8
unknown <seancollins117@gmail.com>
2022-02-20 10:07:09 -0800

    got the dashboard working

--- server.js
+++ server.js
@@ -1,10 +1,7 @@
-const express = require('express');
-const routes = require('./controllers');
 const path = require('path');
-const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
+const express = require('express');
 const session = require('express-session');
-
+const exphbs = require('express-handlebars');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -24,17 +21,19 @@ const sess = {
 
 app.use(session(sess));
 
+const helpers = require('./utils/helpers');
+
+const hbs = exphbs.create({ helpers });
+
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// turn on routes
-app.use(routes);
+app.use(require('./controllers/'));
 
-// turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
--- utils/helpers.js
+++ utils/helpers.js
@@ -1,9 +1,14 @@
-const {format_date} = require('../utils/helpers');
-
 module.exports = {
   format_date: date => {
     return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${new Date(
       date
     ).getFullYear()}`;
+  },
+  format_plural: (word, amount) => {
+    if (amount !== 1) {
+      return `${word}s`;
+    }
+
+    return word;
   }
-}
+};
